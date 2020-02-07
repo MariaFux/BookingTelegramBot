5ed189161686a983f79fb4e2901e328cac7caa98
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-07 17:44:23 +0300

    Added interface for user reservation repo

---
+++ BookingTelegramBot/Repository/IUserReservationRepo.cs
@@ -0,0 +1,18 @@
+﻿using BookingTelegramBot.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.Repository
+{
+    interface IUserReservationRepo
+    {
+        IEnumerable<UserReservation> GetAll();
+        UserReservation GetUserReservationByID(int userReservationID);
+        void Insert(UserReservation userReservation);
+        void Update(UserReservation userReservation);
+        void Delete(int userReservationID);
+        void Save();
+    }
+}
