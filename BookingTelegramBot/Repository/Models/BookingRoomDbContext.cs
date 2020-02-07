b41eec274c0f8ab5723176d77d2d5943f671709b
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-07 17:24:13 +0300

    Added booking room DBCOntext

---
+++ BookingTelegramBot/Repository/Models/BookingRoomDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.Repository.Models
+{
+    public class BookingRoomDbContext : DbContext
+    {
+        public BookingRoomDbContext(DbContextOptions<BookingRoomDbContext> options) : base(options)
+        {
+        }
+
+        protected BookingRoomDbContext()
+        {
+        }
+
+        public DbSet<Parameter> Parameters { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<UserReservation> UsersReservations { get; set; }
+    }
+}
