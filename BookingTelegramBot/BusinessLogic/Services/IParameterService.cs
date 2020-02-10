e85d4714534b9e0d895ea93d5e0322b067426991
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-10 12:31:30 +0300

    Added interface for parameter service

---
+++ BookingTelegramBot/BusinessLogic/Services/IParameterService.cs
@@ -0,0 +1,18 @@
+﻿using BookingTelegramBot.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.BusinessLogic.Services
+{
+    interface IParameterService
+    {
+        IEnumerable<Parameter> GetAll();
+        Parameter GetParameterByID(int parameterID);
+        void Insert(Parameter parameter);
+        void Update(Parameter parameter);
+        void Delete(int parameterID);
+        void Save();
+    }
+}
