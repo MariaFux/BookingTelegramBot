e5ebeb1a3c36674fc476d10b2535258b42188529
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-10 12:33:05 +0300

    Added interface for user reservation service

---
+++ BookingTelegramBot/BusinessLogic/Services/IUserReservationService.cs
@@ -0,0 +1,18 @@
+﻿using BookingTelegramBot.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.BusinessLogic.Services
+{
+    interface IUserReservationService
+    {
+        IEnumerable<UserReservation> GetAll();
+        UserReservation GetUserReservationByID(int userReservationID);
+        void Insert(UserReservation userReservation);
+        void Update(UserReservation userReservation);
+        void Delete(int userReservationID);
+        void Save();
+    }
+}
