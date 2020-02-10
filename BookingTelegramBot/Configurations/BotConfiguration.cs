12b21df622176387f78cd4e12fc958c20dd8725b
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-10 13:46:01 +0300

    Added folder Configurations with class BotConfiguration.cs

---
+++ BookingTelegramBot/Configurations/BotConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.Configurations
+{
+    public class BotConfiguration
+    {
+        public string Token { get; set; }
+    }
+}
