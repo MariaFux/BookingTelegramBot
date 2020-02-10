57d05289b65d76af87bd8e8bc9329eb3aecf2120
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-10 12:33:54 +0300

    Added models in business logic

---
+++ BookingTelegramBot/BusinessLogic/Models/UserReservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.Repository.Models
+{
+    public class UserReservation
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime TimeFrom { get; set; }
+        public DateTime TimeTo { get; set; }
+
+        public int? RoomId { get; set; }
+        public Room Room { get; set; }
+    }
+}
