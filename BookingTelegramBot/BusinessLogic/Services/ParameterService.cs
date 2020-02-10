cb526065a3da2eb69243069e6c151bf68a0ce43c
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-10 12:47:37 +0300

    Adde parameter service

---
+++ BookingTelegramBot/BusinessLogic/Services/ParameterService.cs
@@ -0,0 +1,49 @@
+﻿using BookingTelegramBot.Repository;
+using BookingTelegramBot.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.BusinessLogic.Services
+{
+    public class ParameterService : IParameterService
+    {
+        public ParameterRepo parameterRepo;
+
+        public ParameterService(ParameterRepo parameterRepo)
+        {
+            this.parameterRepo = parameterRepo;
+        }        
+
+        public IEnumerable<Parameter> GetAll()
+        {
+            return parameterRepo.GetAll();
+        }
+
+        public Parameter GetParameterByID(int parameterID)
+        {
+            return parameterRepo.GetParameterByID(parameterID);
+        }
+
+        public void Insert(Parameter parameter)
+        {
+            parameterRepo.Insert(parameter);
+        }        
+
+        public void Update(Parameter parameter)
+        {
+            parameterRepo.Update(parameter);
+        }
+
+        public void Delete(int parameterID)
+        {
+            parameterRepo.Delete(parameterID);
+        }
+
+        public void Save()
+        {
+            parameterRepo.Save();
+        }
+    }
+}
