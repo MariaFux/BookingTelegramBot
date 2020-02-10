b0805ed465bd1fc5fcc8e77ed06e57540e5433fe
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-10 12:30:03 +0300

    Added interface for room service

---
+++ BookingTelegramBot/BusinessLogic/Services/IRoomService.cs
@@ -0,0 +1,18 @@
+﻿using BookingTelegramBot.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.BusinessLogic.Services
+{
+    interface IRoomService
+    {
+        IEnumerable<Room> GetAll();
+        Room GetRoomByID(int roomID);
+        void Inser(Room room);
+        void Update(Room room);
+        void Delete(int roomID);
+        void Save();
+    }
+}
