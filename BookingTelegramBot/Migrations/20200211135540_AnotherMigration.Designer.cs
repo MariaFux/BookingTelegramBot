eb1448f176213a1f0343b21d557bc5a0bc8d65a2
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-11 17:02:19 +0300

    Added another migration

--- BookingTelegramBot/Migrations/20200211135540_AnotherMigration.Designer.cs
+++ BookingTelegramBot/Migrations/20200211135540_AnotherMigration.Designer.cs
@@ -4,14 +4,16 @@ using BookingTelegramBot.Repository.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookingTelegramBot.Migrations
 {
     [DbContext(typeof(BookingRoomDbContext))]
-    partial class BookingRoomDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200211135540_AnotherMigration")]
+    partial class AnotherMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookingTelegramBot/Migrations/20200211135540_AnotherMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookingTelegramBot.Migrations
+{
+    public partial class AnotherMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AnotherDescription",
+                table: "Rooms",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnotherDescription",
+                table: "Rooms");
+        }
+    }
+}
