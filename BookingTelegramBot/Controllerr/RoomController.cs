6b578138e199fca4b516b1133edc44912dec1a2d
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-11 15:53:52 +0300

    Added room controller

---
+++ BookingTelegramBot/Controllerr/RoomController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookingTelegramBot.BusinessLogic.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingTelegramBot.Controllerr
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : Controller
+    {
+        private RoomService roomService;
+
+        public RoomController(RoomService roomService)
+        {
+            this.roomService = roomService;
+        }
+
+        public async void GetRoomById()
+        {
+           var room = roomService.GetRoomByID(3);
+           await Response.WriteAsync(room.Name);
+        }
+    }
+}
