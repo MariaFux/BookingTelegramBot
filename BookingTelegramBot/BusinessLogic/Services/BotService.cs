cadda2163cbad3ca114e8872dc8d2e8cdcd5f0fe
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-12 13:08:51 +0300

    Added bot service

--- BookingTelegramBot/BusinessLogic/Services/BotService.cs
+++ BookingTelegramBot/BusinessLogic/Services/BotService.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using BookingTelegramBot.Configurations;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace BookingTelegramBot.BusinessLogic.Services
 {
-    public class BotService
+    public class BotService : IBotService
+    {
+        private readonly BotConfiguration _config;
+
+        public BotService(IOptions<BotConfiguration> config)
+        {
+            _config = config.Value;
+            Client = new TelegramBotClient(_config.Token);
+        }
+
+        public TelegramBotClient Client { get; }
     {
     }
 }
