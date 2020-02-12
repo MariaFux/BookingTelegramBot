440be806ef039cdff99be4a5658f0168f3d0e24f
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-12 13:41:49 +0300

    Added parameter migration

--- BookingTelegramBot/Migrations/20200212103728_ParameterMigration.Designer.cs
+++ BookingTelegramBot/Migrations/20200212103728_ParameterMigration.Designer.cs
@@ -4,14 +4,16 @@ using BookingTelegramBot.Repository.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookingTelegramBot.Migrations
 {
     [DbContext(typeof(BookingRoomDbContext))]
-    partial class BookingRoomDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200212103728_ParameterMigration")]
+    partial class ParameterMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
