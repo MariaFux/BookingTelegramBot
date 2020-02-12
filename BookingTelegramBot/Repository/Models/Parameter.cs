9df957b42081020cf2923823263f2d140c4dbf35
MariaFux <41433307+MariaFux@users.noreply.github.com>
2020-02-12 14:28:01 +0300

    Data access (#1)
    * Added Parameter UserReservation and Room entities
    * Added booking room DBCOntext
    * Added interface for parameter repository
    * Added interface for room repository
    * Added interface for user reservation repo
    * Added parameter repository
    * Added room repository
    * Adde users reservation repository
    * Added connection string in appserrings.json
    * Added connection string in Startup.cs
    * Added new startup configurations
    * room id connection removed from Parameter.cs and UserReservation

---
+++ BookingTelegramBot/Repository/Models/Parameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.Repository.Models
+{
+    public class Parameter
+    {
+        public int Id { get; set; }
+        public string NameOfParameter { get; set; }
+    }
+}
