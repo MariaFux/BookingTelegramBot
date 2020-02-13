34918381983f91f626c8470e62aa98f2b0ad02dc
MariaFux <41433307+MariaFux@users.noreply.github.com>
2020-02-13 15:03:44 +0300

    Business logic (#6)
    * Added interface for parameter repository
    * Added interface for room repository
    * Added interface for user reservation repo
    * Added parameter repository
    * Added room repository
    * Adde users reservation repository
    * Added interface for room service
    * Added interface for parameter service
    * Added interface for user reservation service
    * Added models in business logic
    * Added room service
    * Adde parameter service
    * Added user reservation service
    * Added connection string in appserrings.json
    * Added connection string in Startup.cs
    * Added new startup configurations
    * Save changes BookingTelegramBot.csproj
    * Added new startup configurations and map controller route
    * Added interface for bot service
    * Added empty bot service
    * Save changes BookingTelegramBot.csproj
    * Added interface for update service and update service
    * room id connection removed from Parameter.cs and UserReservation
    * Added bot configuration
    * Added bot service
    * Added update service
    * Added bot configuration in appsettings.json
    * Added update and bot services and bot configuration in startup

---
+++ BookingTelegramBot/BusinessLogic/Models/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.BusinessLogic.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Parameter> Parameters { get; set; }
+        public ICollection<UserReservation> UsersReservations { get; set; }
+
+        public Room()
+        {
+            Parameters = new List<Parameter>();
+            UsersReservations = new List<UserReservation>();
+        }
+    }
+}
---
+++ BookingTelegramBot/BusinessLogic/Services/BotService.cs
@@ -0,0 +1,23 @@
+﻿using BookingTelegramBot.Configurations;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace BookingTelegramBot.BusinessLogic.Services
+{
+    public class BotService : IBotService
+    {
+        private readonly BotConfiguration _config;
+
+        public BotService(IOptions<BotConfiguration> config)
+        {
+            _config = config.Value;
+            Client = new TelegramBotClient(_config.Token);
+        }
+
+        public TelegramBotClient Client { get; }
+    }
+}
