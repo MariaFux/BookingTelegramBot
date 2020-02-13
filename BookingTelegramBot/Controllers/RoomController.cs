0bbf83745d32d2581853c37f3a5520ffaec5428d
MariaFux <41433307+MariaFux@users.noreply.github.com>
2020-02-13 21:59:53 +0300

    Added MessageController

---
+++ BookingTelegramBot/Controllers/RoomController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookingTelegramBot.BLL.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingTelegramBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : Controller
+    {
+        private RoomService roomService;
+
+        public RoomController(RoomService roomService)
+        {
+            this.roomService = roomService;
+        }
+        
+        [Route("{id}")]
+        public async void GetRoomById(int id)
+        {
+           var room = roomService.GetRoomById(id);
+           
+            await Response.WriteAsync(room.Name + " Description: " + room.Description + " Another description: " + room.AnotherDescription);
+        }
+    }
+}
