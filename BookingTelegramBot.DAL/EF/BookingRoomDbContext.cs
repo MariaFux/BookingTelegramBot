459c76fccadb1876a32b9bf8bc1d4a3273745f77
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-17 14:24:59 +0300

    Added override method OnModelCreating

--- BookingTelegramBot.DAL/EF/BookingRoomDbContext.cs
+++ BookingTelegramBot.DAL/EF/BookingRoomDbContext.cs
@@ -19,5 +19,34 @@ namespace BookingTelegramBot.DAL.EF
         public DbSet<Parameter> Parameters { get; set; }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<UserReservation> UsersReservations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RoomParameter>()
+                .HasKey(t => new { t.RoomId, t.ParameterId });
+
+            modelBuilder.Entity<RoomParameter>()
+                .HasOne(rp => rp.Room)
+                .WithMany(r => r.RoomParameters)
+                .HasForeignKey(rp => rp.RoomId);
+
+            modelBuilder.Entity<RoomParameter>()
+                .HasOne(rp => rp.Parameter)
+                .WithMany(p => p.RoomParameters)
+                .HasForeignKey(rp => rp.ParameterId);
+
+            modelBuilder.Entity<RoomUserReservation>()
+                .HasKey(t => new { t.RoomId, t.UserReservationId });
+
+            modelBuilder.Entity<RoomUserReservation>()
+                .HasOne(rp => rp.Room)
+                .WithMany(r => r.RoomUserReservations)
+                .HasForeignKey(rp => rp.RoomId);
+
+            modelBuilder.Entity<RoomUserReservation>()
+                .HasOne(rp => rp.UserReservation)
+                .WithMany(p => p.RoomUserReservations)
+                .HasForeignKey(rp => rp.UserReservationId);
+        }
     }
 }
