05f554e2811605b4f94422d16c242bbf1c85eb87
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-20 15:51:27 +0300

    Changed field AnotherDescriprion to NumberOfPersons

--- BookingTelegramBot.DAL/Entities/Room.cs
+++ BookingTelegramBot.DAL/Entities/Room.cs
@@ -9,7 +9,7 @@ namespace BookingTelegramBot.DAL.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string AnotherDescription { get; set; }
+        public int NumberOfPersons { get; set; }
         public ICollection<RoomParameter> RoomParameters { get; set; } = new List<RoomParameter>();
         public ICollection<RoomUserReservation> RoomUserReservations { get; set; } = new List<RoomUserReservation>();
     }
