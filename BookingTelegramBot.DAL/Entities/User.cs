dfe321a6abfc1d57e9cf07bf5045b5722dc1c620
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-21 17:54:33 +0300

    Added UserRole model

--- BookingTelegramBot.DAL/Entities/User.cs
+++ BookingTelegramBot.DAL/Entities/User.cs
@@ -7,7 +7,9 @@ namespace BookingTelegramBot.DAL.Entities
     public class User
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
     }
 }
