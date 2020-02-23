a3ae7be2e7e366323cb3dff57c9b83523301de6b
MariaFux <41433307+MariaFux@users.noreply.github.com>
2020-02-23 13:56:02 +0300

    Added AccountController

---
+++ BookingTelegramBot/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BookingTelegramBot.BLL.Services;
+using BookingTelegramBot.DAL.Entities;
+using BookingTelegramBot.DAL.Repositories;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingTelegramBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private UserService userService;
+        public AccountController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        [Route("find/{id}")]
+        public async Task FindUser(int id)
+        {
+            var user = await userService.FindByUserIdAsync(id);
+            await Authenticate(user.TelegramName);
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+            
+            ClaimsIdentity uid = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(uid));
+        }
+    }
+}
