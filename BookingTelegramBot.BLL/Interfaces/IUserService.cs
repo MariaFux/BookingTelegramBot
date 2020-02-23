0d35a0a5b507afd5fec84bb0b1e10b08fb9402fa
MariaFux <41433307+MariaFux@users.noreply.github.com>
2020-02-23 13:52:03 +0300

    Added UserService interface and UserServise

---
+++ BookingTelegramBot.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using BookingTelegramBot.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.BLL.Interfaces
+{
+    interface IUserService
+    {
+        Task<UserDTO> FindByUserIdAsync(int userId);
+    }
+}
