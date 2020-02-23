2319b9f12dd68cdebfa20772b014479a389ce4b4
MariaFux <41433307+MariaFux@users.noreply.github.com>
2020-02-23 13:49:18 +0300

    Added UserDTO

---
+++ BookingTelegramBot.BLL/DTO/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingTelegramBot.BLL.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string TelegramName { get; set; }
+    }
+}
