725d97c3905a45c8ff601a528ec4a7d274267a97
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-26 15:14:26 +0300

    Changed RoleDTO

--- BookingTelegramBot.BLL/DTO/RoleDTO.cs
+++ BookingTelegramBot.BLL/DTO/RoleDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookingTelegramBot.DAL.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,7 +8,7 @@ namespace BookingTelegramBot.BLL.DTO
     public class RoleDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public Roles UserRole { get; set; }
         public List<UserDTO> Users { get; set; }
     }
 }
