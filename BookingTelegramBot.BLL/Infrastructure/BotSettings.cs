f0ba212686f51e1feddeb97890166fb110a9206f
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-02-26 15:13:35 +0300

    Changed BotSettings

--- BookingTelegramBot.BLL/Infrastructure/BotSettings.cs
+++ BookingTelegramBot.BLL/Infrastructure/BotSettings.cs
@@ -4,10 +4,10 @@ using System.Text;
 
 namespace BookingTelegramBot.BLL.Infrastructure
 {
-    public static class BotSettings
+    public class BotSettings
     {
-        public static string Token { get; set; } = "Token";
-        public static string Name { get; set; } = "Name";
-        public static string Url { get; set; } = "Ngrok/{0}";
+        public string Token { get; set; } = "Token";
+        public string Name { get; set; } = "Name";
+        public string BaseUrl { get; set; } = "Ngrok";
     }
 }
