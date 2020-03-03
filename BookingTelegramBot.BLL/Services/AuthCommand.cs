2353104f8882e275eff6979f256005c8992802ad
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-03-03 15:15:56 +0300

    Changed AuthCommand class

--- BookingTelegramBot.BLL/Services/AuthCommand.cs
+++ BookingTelegramBot.BLL/Services/AuthCommand.cs
@@ -1,4 +1,5 @@
-﻿using BookingTelegramBot.BLL.Interfaces;
+﻿using BookingTelegramBot.BLL.DTO;
+using BookingTelegramBot.BLL.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,6 +12,13 @@ namespace BookingTelegramBot.BLL.Services
 {
     public class AuthCommand : ICommand
     {
+        private readonly UserService _userService;
+
+        public AuthCommand(UserService userService)
+        {
+            _userService = userService;
+        }
+
         public string Name => @"/auth";
 
         public bool Contains(Message message)
@@ -23,7 +31,25 @@ namespace BookingTelegramBot.BLL.Services
         public async Task Execute(Message message, TelegramBotClient client)
         {
             var chatId = message.Chat.Id;
-            await client.SendTextMessageAsync(chatId, "Введи свое имя!");
+            var telegramId = message.From.Id;
+            var user = await _userService.FindByTelegramIdAsync(telegramId);
+            if (user != null)
+            {
+                await client.SendTextMessageAsync(chatId, @"Теперь вы можете использовать полный перечень команд:
+/free - Позволяет найти свободные комнаты исходя из введенных данных(дата, время, количество человек)
+/all - Список всех доступных комнат
+/order - Забронировать подходящую комнату");
+            }
+            else
+            {
+                var userDTO = new UserDTO() { TelegramId = telegramId, RoleId = 2 };
+                _userService.Insert(userDTO);
+                await _userService.SaveAsync();
+                await client.SendTextMessageAsync(chatId, "Теперь вы можете использовать полный перечень команд:" +
+                    "/free - Позволяет найти свободные комнаты исходя из введенных данных(дата, время, количество человек)" +
+                    "/all - Список всех доступных комнат" +
+                    "/order - Забронировать подходящую комнату");
+            }
         }
     }
 }
