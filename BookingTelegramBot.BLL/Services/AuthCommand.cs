342d394038f551a249ddb7cf320e11aae399a566
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-03-09 17:44:27 +0300

    Modified auth command

--- BookingTelegramBot.BLL/Services/AuthCommand.cs
+++ BookingTelegramBot.BLL/Services/AuthCommand.cs
@@ -2,6 +2,7 @@
 using BookingTelegramBot.BLL.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
@@ -30,25 +31,31 @@ namespace BookingTelegramBot.BLL.Services
 
         public async Task Execute(Message message, TelegramBotClient client)
         {
-            var chatId = message.Chat.Id;
-            var telegramId = message.From.Id;
-            var user = await _userService.FindByTelegramIdAsync(telegramId);
-            if (user != null)
-            {
-                await client.SendTextMessageAsync(chatId, @"Теперь вы можете использовать полный перечень команд:
+            var answer = @"Теперь вы можете использовать полный перечень команд:
 /free - Позволяет найти свободные комнаты исходя из введенных данных(дата, время, количество человек)
 /all - Список всех доступных комнат
-/order - Забронировать подходящую комнату");
+/order - Забронировать подходящую комнату";
+
+            var chatId = message.Chat.Id;
+            var telegramId = message.From.Id;
+
+            var user = await _userService.FindByTelegramIdAsync(telegramId);
+
+            if (user != null && user.Role.UserRole.ToString() == "admin")
+            {
+                answer += "\n/create - Позволяет добавить новую комнату";
+                await client.SendTextMessageAsync(chatId, answer);
+            }
+            else if (user != null && user.Role.UserRole.ToString() == "user")
+            {
+                await client.SendTextMessageAsync(chatId, answer);
             }
             else
             {
                 var userDTO = new UserDTO() { TelegramId = telegramId, RoleId = 2 };
                 _userService.Insert(userDTO);
                 await _userService.SaveAsync();
-                await client.SendTextMessageAsync(chatId, "Теперь вы можете использовать полный перечень команд:" +
-                    "/free - Позволяет найти свободные комнаты исходя из введенных данных(дата, время, количество человек)" +
-                    "/all - Список всех доступных комнат" +
-                    "/order - Забронировать подходящую комнату");
+                await client.SendTextMessageAsync(chatId, answer);
             }
         }
     }
