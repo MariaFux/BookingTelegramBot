0b27df5bf367dbead040954ad64c20d7e1f0fdde
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-03-10 16:00:45 +0300

    Added new commands to Bot

--- BookingTelegramBot.BLL/Infrastructure/Bot.cs
+++ BookingTelegramBot.BLL/Infrastructure/Bot.cs
@@ -1,5 +1,5 @@
 ﻿using BookingTelegramBot.BLL.Interfaces;
-using BookingTelegramBot.BLL.Services;
+using BookingTelegramBot.BLL.Services.Commands;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
@@ -14,16 +14,29 @@ namespace BookingTelegramBot.BLL.Infrastructure
         private static TelegramBotClient _botClient;
         private static List<ICommand> _commandsList = new List<ICommand>();
         private readonly BotSettings _settings;
+
         private readonly AuthCommand _authCommand;
         private readonly FreeCommand _freeCommand;
         private readonly CreateCommand _createCommand;
+        private readonly UpdateCommand _updateCommand;
+        private readonly GetAllRoomsCommand _getAllRoomsCommand;
+        private readonly DeleteCommand _deleteCommand;
+        private readonly GetAllUsersCommand _getAllUsersCommand;
+        private readonly SetRoleCommand _setRoleCommand;
 
-        public Bot(IOptions<BotSettings> settings, AuthCommand authCommand, FreeCommand freeCommand, CreateCommand createCommand)
+        public Bot(IOptions<BotSettings> settings, AuthCommand authCommand, FreeCommand freeCommand, CreateCommand createCommand, 
+            UpdateCommand updateCommand, GetAllRoomsCommand getAllRoomsCommand, DeleteCommand deleteCommand, GetAllUsersCommand getAllUsersCommand,
+            SetRoleCommand setRoleCommand)
         {
             _settings = settings.Value;
             _authCommand = authCommand;
             _freeCommand = freeCommand;
             _createCommand = createCommand;
+            _updateCommand = updateCommand;
+            _getAllRoomsCommand = getAllRoomsCommand;
+            _deleteCommand = deleteCommand;
+            _getAllUsersCommand = getAllUsersCommand;
+            _setRoleCommand = setRoleCommand;
         }
 
         public IReadOnlyList<ICommand> Commands => _commandsList.AsReadOnly();
@@ -47,6 +60,11 @@ namespace BookingTelegramBot.BLL.Infrastructure
             _commandsList.Add(_authCommand);
             _commandsList.Add(_freeCommand);
             _commandsList.Add(_createCommand);
+            _commandsList.Add(_updateCommand);
+            _commandsList.Add(_getAllRoomsCommand);
+            _commandsList.Add(_deleteCommand);
+            _commandsList.Add(_getAllUsersCommand);
+            _commandsList.Add(_setRoleCommand);
             //TODO: Add more commands
 
             var botClient = new TelegramBotClient(_settings.Token);
