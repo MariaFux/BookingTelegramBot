c370074ac51aa08fa63d09b84fea962928547242
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-03-16 13:10:01 +0300

    Added room parameter service

---
+++ BookingTelegramBot.BLL/Interfaces/IRoomParameterService.cs
@@ -0,0 +1,14 @@
+﻿using BookingTelegramBot.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.BLL.Interfaces
+{
+    interface IRoomParameterService
+    {
+        void Insert(RoomParameterDTO roomParameterDTO);
+        Task SaveAsync();
+    }
+}
