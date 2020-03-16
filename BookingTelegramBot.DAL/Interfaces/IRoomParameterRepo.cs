5517044c34525ad0bc5f5ac9f678e135bdf8ca69
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-03-16 13:08:44 +0300

    Added room parameter repository

---
+++ BookingTelegramBot.DAL/Interfaces/IRoomParameterRepo.cs
@@ -0,0 +1,14 @@
+﻿using BookingTelegramBot.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.DAL.Interfaces
+{
+    interface IRoomParameterRepo
+    {
+        void Insert(RoomParameter roomParameter);
+        Task SaveAsync();
+    }
+}
---
+++ BookingTelegramBot.DAL/Repositories/RoomParameterRepo.cs
@@ -0,0 +1,30 @@
+﻿using BookingTelegramBot.DAL.EF;
+using BookingTelegramBot.DAL.Entities;
+using BookingTelegramBot.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingTelegramBot.DAL.Repositories
+{
+    public class RoomParameterRepo : IRoomParameterRepo
+    {
+        private readonly BookingRoomDbContext _context;
+
+        public RoomParameterRepo(BookingRoomDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Insert(RoomParameter roomParameter)
+        {
+            _context.RoomsParameters.Add(roomParameter);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
