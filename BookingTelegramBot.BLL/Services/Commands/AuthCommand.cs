e110aa9e9717a28709259ebd387ecdfad0378527
Maryia Puhachova <maryiapuhachova@gmail.com>
2020-03-16 16:20:58 +0300

    Added templates for commands

--- BookingTelegramBot.BLL/Services/Commands/AuthCommand.cs
+++ BookingTelegramBot.BLL/Services/Commands/AuthCommand.cs
@@ -31,8 +31,10 @@ namespace BookingTelegramBot.BLL.Services.Commands
 
         public async Task Execute(Message message, TelegramBotClient client)
         {
-            var answer = @"Теперь вы можете использовать полный перечень команд:
+            var answer = @"Теперь вы можете использовать полный *перечень команд*:
 /free - Позволяет найти свободные комнаты исходя из введенных данных(дата, время, количество человек)
+*Шаблон:* _/free Date Time NumberOfPersons_
+`/free 2020-02-18 15:31 5`
 /getallrooms - Список всех доступных комнат
 /order - Забронировать подходящую комнату";
 
@@ -44,18 +46,50 @@ namespace BookingTelegramBot.BLL.Services.Commands
             if (user != null && user.Role.UserRole.ToString() == "admin")
             {
                 answer += "\n\n*Admin commands:*";
+
                 answer += "\n/createroom - Позволяет добавить новую комнату";
+                answer += "\n`Шаблон:";
+                answer += "\n/createroom, Name, Descr, PersonsCount";
+                answer += "\n/createroom, Room 1, First, 10`";
+
                 answer += "\n/updateroom - Позволяет изменить существующую комнату";
+                answer += "\n_Шаблон:_ _/updateroom, RoomId, Name, Description, NumberOfPersons_";
+                answer += "\n`/updateroom, 1, Room 1, First room, 10`";
+
                 answer += "\n/deleteroom - Позволяет удалить существующую комнату";
+                answer += "\n_Шаблон:_ _/deleteroom RoomId_";
+                answer += "\n`/deleteroom 1`";
+
                 answer += "\n/getallusers - Список всех пользователей";
+
                 answer += "\n/setrole - Позволяет изменить роль пользователя";
+                answer += "\n_Шаблон:_ _/setrole UserId RoleId UserTelegramId_";
+                answer += "\n`/setrole 1 1 111111111`";
+
                 answer += "\n/createparameter - Позволяет добавить новый параметер";
+                answer += "\n_Шаблон:_ _/createparameter ParameterName_";
+                answer += "\n`/createparameter Projector`";
+
                 answer += "\n/updateparameter - Позволяет изменить имеющийся параметер";
+                answer += "\n_Шаблон:_ _/updateparameter ParameterId ParameterName_";
+                answer += "\n`/updateparameter 1 Display`";
+
                 answer += "\n/getallparameters - Список всех параметров";
+
                 answer += "\n/deleteparameter - Позволяет удалить параметер";
+                answer += "\n_Шаблон:_ _/deleteparameter ParameterId_";
+                answer += "\n`/deleteparameter 1`";
+
                 answer += "\n/allroomsparameters - Список всех комнат с параметрами";
+
                 answer += "\n/addroomsparameters - Позволяет добавить параметры к комнате";
+                answer += "\n_Шаблон:_ _/addroomsparameters RoomId ParameterIds_";
+                answer += "\n`/addroomsparameters 1 1 2 3 4 5`";
+
                 answer += "\n/roomsparametersdelete - Позволяет убрать параметер из комнаты";
+                answer += "\n_Шаблон:_ _/roomsparametersdelete RoomId ParameterId_";
+                answer += "\n`/roomsparametersdelete 1 4`";
+
                 await client.SendTextMessageAsync(chatId, answer, ParseMode.Markdown);
             }
             else if (user != null && user.Role.UserRole.ToString() == "user")
