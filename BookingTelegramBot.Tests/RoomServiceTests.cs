75632e7fc62c81d81ecda2da5951aeb397ca8e28
MariaFux <41433307+MariaFux@users.noreply.github.com>
2020-03-18 13:33:19 +0300

    Added Startup and RoomServiceTests class

---
+++ BookingTelegramBot.Tests/RoomServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using BookingTelegramBot.BLL.DTO;
+using BookingTelegramBot.BLL.Services;
+using BookingTelegramBot.DAL.EF;
+using BookingTelegramBot.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BookingTelegramBot.Tests
+{
+    public class RoomServiceTests
+    {
+        private IMapper _mapper;
+        
+        public RoomServiceTests(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [Fact]
+        public async Task GetRoomByIdAsync()
+        {
+            var options = new DbContextOptionsBuilder<BookingRoomDbContext>().UseInMemoryDatabase(databaseName: "BookingRoom").Options;
+
+            using (var context = new BookingRoomDbContext(options))
+            {
+                var repo = new RoomRepo(context);
+                var room = await repo.GetRoomByIdAsync(1);
+
+                var mockMapper = _mapper.Map<RoomDTO>(room);
+
+                var service = new RoomService(repo, _mapper);
+                var result = await service.GetRoomByIdAsync(1);
+
+                Assert.Equal(mockMapper.Name, result.Name);
+                Assert.Equal(mockMapper.NumberOfPersons, result.NumberOfPersons);
+                Assert.Equal(mockMapper.RoomParameters.Count, result.RoomParameters.Count);
+            }
+        }   
+    }    
+}
---
+++ BookingTelegramBot.Tests/Startup.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BookingTelegramBot.BLL.Mapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.DependencyInjection;
+
+[assembly: TestFramework("BookingTelegramBot.Tests.Startup", "BookingTelegramBot.Tests")]
+namespace BookingTelegramBot.Tests
+{
+    public class Startup : DependencyInjectionTestFramework
+    {
+        public Startup(IMessageSink messageSink) : base(messageSink)
+        {
+        }
+
+        protected void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(x => x.AddProfile(new MappingProfile()), typeof(Startup));
+        }
+
+        protected override IHostBuilder CreateHostBuilder(AssemblyName assemblyName) =>
+        base.CreateHostBuilder(assemblyName)
+            .ConfigureServices(ConfigureServices);
+    }
+}
