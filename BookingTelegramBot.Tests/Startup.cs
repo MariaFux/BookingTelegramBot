f9468e28e593105955900316e58c072e50a00646
MariaFux <41433307+MariaFux@users.noreply.github.com>
2020-03-19 11:33:19 +0300

    Added SeedData method to Startup class

--- BookingTelegramBot.Tests/Startup.cs
+++ BookingTelegramBot.Tests/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using BookingTelegramBot.BLL.Mapper;
+using BookingTelegramBot.DAL.EF;
+using BookingTelegramBot.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Reflection;
@@ -13,16 +16,29 @@ namespace BookingTelegramBot.Tests
     public class Startup : DependencyInjectionTestFramework
     {
         public Startup(IMessageSink messageSink) : base(messageSink)
-        {
+        {           
         }
 
         protected void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<RoomRepo>();
             services.AddAutoMapper(x => x.AddProfile(new MappingProfile()), typeof(Startup));
+            services.AddDbContext<BookingRoomDbContext>(options => options.UseInMemoryDatabase(databaseName: "BookingRoom"));
+            SeedData(services);
         }
 
         protected override IHostBuilder CreateHostBuilder(AssemblyName assemblyName) =>
         base.CreateHostBuilder(assemblyName)
             .ConfigureServices(ConfigureServices);
+
+        private void SeedData(IServiceCollection services)
+        {
+            using (var scope = services.BuildServiceProvider().CreateScope())
+            {
+                var servicesIn = scope.ServiceProvider;
+                var context = servicesIn.GetRequiredService<BookingRoomDbContext>();
+                BookingRoomDbContextInitialize.Initialize(servicesIn);
+            }
+        }
     }
 }
