cedecb4a19747c6fe6aaf7829acc80b2939b1b0a
MariaFux <41433307+MariaFux@users.noreply.github.com>
2020-03-23 22:41:43 +0300

    Added TelegramId field to UserReservation entity

--- BookingTelegramBot.DAL/Entities/UserReservation.cs
+++ BookingTelegramBot.DAL/Entities/UserReservation.cs
@@ -9,6 +9,7 @@ namespace BookingTelegramBot.DAL.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int TelegramId { get; set; }
         public DateTime DateTimeFrom { get; set; }
         public DateTime DateTimeTo { get; set; }
         public ICollection<RoomUserReservation> RoomUserReservations { get; set; } = new List<RoomUserReservation>();
